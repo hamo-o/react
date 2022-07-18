13e0ef1f8c22a5ff7eec5d200e3de73c00efb0fe
hamo-o <yheel369@gmail.com>
2022-07-18 12:43:15 +0900

    6-2

--- nomadcoders/src/App.js
+++ nomadcoders/src/App.js
@@ -2,16 +2,31 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setCounter] = useState(0);
+  const [keyword, setKeyword] = useState("");
   const myClick = () => setCounter((prev) => prev + 1);
+  const myChange = (event) => setKeyword(event.target.value);
 
-  console.log("I run all the time");
   const IRunOnlyOnce = () => {
     console.log("I run only once");
   };
   useEffect(IRunOnlyOnce, []);
 
+  useEffect(() => {
+    console.log("I run when 'keyword' changes.");
+  }, [keyword]);
+
+  useEffect(() => {
+    console.log("I run when 'counter' changes.");
+  }, [counter]);
+
   return (
     <div>
+      <input
+        value={keyword}
+        onChange={myChange}
+        type="text"
+        placeholder="Search here..."
+      />
       <h1>{counter}</h1>
       <button onClick={myClick}>click me</button>
     </div>
