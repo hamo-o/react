6b0cbccfe0fc2551eff117ae33ceb90adfb2b67f
hamo-o <yheel369@gmail.com>
2022-07-18 14:14:35 +0900

    7-0

--- nomadcoders/src/App.js
+++ nomadcoders/src/App.js
@@ -1,20 +1,32 @@
-import { useEffect, useState } from "react";
-
-function Hello() {
-  useEffect(() => {
-    console.log("created! ;)");
-    return () => console.log("destroyed :(");
-  }, []);
-  return <h1>Hello</h1>;
-}
+import { useState } from "react";
 
 function App() {
-  const [showing, setShowing] = useState(false);
-  const myClick = () => setShowing((prev) => !prev);
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+
+  const myChange = (event) => {
+    setToDo(event.target.value);
+  };
+  const mySubmit = (event) => {
+    event.preventDefault(); // 버튼 눌렀을 때 form 이 refreshing 되는 것을 막아준다
+    if (toDo === "") {
+      return;
+    }
+    setToDos((curArr) => [toDo, ...curArr]);
+    setToDo("");
+  };
   return (
     <div>
-      {showing ? <Hello /> : null}
-      <button onClick={myClick}>{showing ? "Hide" : "Show"}</button>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={mySubmit} action="">
+        <input
+          onChange={myChange}
+          value={toDo}
+          type="text"
+          placeholder="Write your to do..."
+        />
+        <button>Add To Do</button>
+      </form>
     </div>
   );
 }
