5f0126a8ba868ac3818ed3790e13198efd1b6ab0
hamo-o <yheel369@gmail.com>
2022-07-18 11:56:51 +0900

    6-1

--- nomadcoders/src/App.js
+++ nomadcoders/src/App.js
@@ -1,9 +1,15 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setCounter] = useState(0);
   const myClick = () => setCounter((prev) => prev + 1);
-  console.log("call an api");
+
+  console.log("I run all the time");
+  const IRunOnlyOnce = () => {
+    console.log("I run only once");
+  };
+  useEffect(IRunOnlyOnce, []);
+
   return (
     <div>
       <h1>{counter}</h1>
